444c0991d68d4a9d511d51a9edb43b109155acc1
Jesus-Arman0 <vh0105032024@unab.edu.sv>
2024-11-12 22:43:52 -0600

    diseño imagen html

---
+++ Productos/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10,decimal_places=2)
+    descripcion = models.TextField() 
+    unidad = models.IntegerField()
+    disponible = models.BooleanField(default=True)
+    image = models.ImageField(upload_to="media/")
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    telefono = models.CharField(max_length=15)
+    direccion = models.CharField(max_length=255)
+    ciudad = models.CharField(max_length=50)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+# Create your models here.
